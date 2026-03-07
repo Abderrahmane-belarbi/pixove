17d6ea5214a8242c4fa31ca4160cd2879dbc9d88
Abderrahmane Belarbi <abdoustart007@gmail.com>
2026-03-07 08:10:43 +0100

    init

---
+++ app/about.tsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export default function About() {
+  return <Text>Welcome in my about page</Text>;
+}
---
+++ app/index.tsx
@@ -0,0 +1,20 @@
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text className="text-green-400 bg-gray-300">Home Page</Text>
+      <Link href="/about">About</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
