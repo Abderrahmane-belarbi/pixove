e25313002901905e713fea64fc393d5167101fd3
Abderrahmane Belarbi <abdoustart007@gmail.com>
2026-03-10 21:58:05 +0100

    delete origins cors for react native

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,26 +1,12 @@
 import cookieParser from "cookie-parser";
-import cors from "cors";
 import "dotenv/config";
 import express from "express";
-import path from "path";
 import { connectToDatabase } from "./database/connect-to-database";
 import authRoutes from "./routes/auth-routes";
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
-const allowedOrigins = ["http://localhost:3000", process.env.CLIENT_URL].filter(
-  Boolean,
-)[0];
-
-app.use(
-  cors({
-    origin: allowedOrigins,
-    credentials: true, // to allow cross-origin requests
-  }),
-);
-
 // Express middleware that parses incoming JSON request bodies.
 app.use(express.json()); // to parse incoming requests with JSON payloads (req.body)
 // You’re telling Express: For every incoming request, if the Content-Type is application/json, parse the body.
@@ -28,14 +14,6 @@ app.use(express.json()); // to parse incoming requests with JSON payloads (req.b
 app.use(cookieParser()); // to parse cookies from incoming requests (req.cookies)
 
 app.use("/api/auth", authRoutes);
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  // Express 5 uses path-to-regexp v8, where catch-all wildcards must be named.
-  app.get("/{*splat}", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
-}
 
 async function start() {
   await connectToDatabase();
